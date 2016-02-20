09b5dc26e4c4768e61d7960ad21f272b39e3ea90
Pavel Sukhov <pasukhov@yandex-team.ru>
2016-02-20 23:38:03 +0300

    install owner

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ export LOCATE=$(cat ./res/location)
 ln -s ./res/config_${LOCATE}.js ./res/config.js
 ln -s ./res/config_${LOCATE}.json ./res/config.json
 
+chown pasukhov:pasukhov ${PWD}/res/config.js
+chown pasukhov:pasukhov ${PWD}/res/config.json
+
 if [ "${LOCATE}" == "prod" ];
 then
     cp -f ./upstart.conf /etc/init/hugs.conf
